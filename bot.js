0dbbe736b27c6414dc284a61d6051a1464eadacd
Zetra88 <41370005+Zetra88@users.noreply.github.com>
2018-07-18 19:15:07 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,14 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const embed = new Discord.RichEmbed();
+
 client.on("ready", () => {
   console.log("I am ready!");
+});
+
+client.on("message", (message) => {
+  if (message.content.startsWith("ping")) {
+    message.channel.send("pong!");
+  }
 });
 
 client.login(process.env.BOT_TOKEN);
